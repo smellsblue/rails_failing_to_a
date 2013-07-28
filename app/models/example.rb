3cbb07888657ae49a2ec062d539668a4c9f5430c
Mike Virata-Stone <mike@virata-stone.com>
2013-07-27 20:24:44 -0700

    Add the tests that I expect to pass.

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,2 +1,24 @@
 class Example < ActiveRecord::Base
+  scope :scope_without_to_a, -> { where(:description => "An Example").order(:id) }
+  scope :scope_with_to_a, -> { where(:description => "An Example").order(:id).to_a }
+  scope :scope_to_a, -> { to_a }
+  scope :scope_all, -> { all }
+
+  class << self
+    def method_without_to_a
+      where(:description => "An Example").order(:id)
+    end
+
+    def method_with_to_a
+      where(:description => "An Example").order(:id).to_a
+    end
+
+    def method_to_a
+      to_a
+    end
+
+    def method_all
+      all
+    end
+  end
 end
--- test/models/example_test.rb
+++ test/models/example_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class ExampleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "to_a outside of any scope" do
+    assert_equal [examples(:one), examples(:two)], Example.where(:description => "An Example").order(:id).to_a
+  end
+
+  test "to_a outside named scope" do
+    assert_equal [examples(:one), examples(:two)], Example.scope_without_to_a.to_a
+  end
+
+  test "to_a outside method scope" do
+    assert_equal [examples(:one), examples(:two)], Example.method_without_to_a.to_a
+  end
+
+  test "to_a in a named scope" do
+    assert_equal [examples(:one), examples(:two)], Example.scope_with_to_a
+  end
+
+  test "to_a in a method scope" do
+    assert_equal [examples(:one), examples(:two)], Example.method_with_to_a
+  end
+
+  test "to_a named scope" do
+    assert_equal [examples(:one), examples(:two)], Example.scope_without_to_a.scope_to_a
+  end
+
+  test "to_a method scope" do
+    assert_equal [examples(:one), examples(:two)], Example.method_without_to_a.method_to_a
+  end
+
+  test "all named scope" do
+    assert_equal [examples(:one), examples(:two)], Example.scope_without_to_a.scope_all
+  end
+
+  test "all method scope" do
+    assert_equal [examples(:one), examples(:two)], Example.method_without_to_a.method_all
+  end
 end
